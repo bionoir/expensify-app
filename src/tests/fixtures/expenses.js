f219ae2ad5cd101fea12b092d1d8516276896cac
Fabio Dalmasso <fabio.dalmasso@gmail.com>
2021-02-11 17:53:03 +0100

    Setup formatting for amount and createdAt

--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -4,18 +4,18 @@ export default [{
     id: '1',
     description: 'Gum',
     note: '',
-    amount: 195,
+    amount: 1950,
     createdAt: 0
 },{
     id: '2',
     description: 'Rent',
     note: '',
-    amount: 109500,
+    amount: 1090500,
     createdAt: moment(0).subtract(4, 'days').valueOf()
 },{
     id: '3',
     description: 'Credit Card',
     note: '',
-    amount: 4500,
+    amount: 45000,
     createdAt:  moment(0).add(4, 'days').valueOf()
 }]
